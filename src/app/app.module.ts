6f17288adf4b2f1eef7d18b72e11ed0515f2be21
CaiodelaRosa <caio.gomiero@hotmail.com>
2023-10-25 19:40:21 -0300

    Added all components and stylizing main-title component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MainTitleComponent } from './components/main-title/main-title.component';
+import { MainNewsComponent } from './components/main-news/main-news.component';
+import { SecondaryNewsComponent } from './components/secondary-news/secondary-news.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MainTitleComponent,
+    MainNewsComponent,
+    SecondaryNewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
